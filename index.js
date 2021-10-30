d46a67407dd1c14d691a87354928638799baa0a5
Ikenna Nwauwa <icknwauwa15@gmail.com>
2021-10-30 15:18:56 -0400

    added questions and licenses

--- index.js
+++ index.js
@@ -1,13 +1,88 @@
 // TODO: Include packages needed for this application
-const fs = require('fs');
+const fs = require("fs");
+const util = require("util");
+const inquirer = require("inquirer");
+const generateMarkdown = require("./utils/generateMarkdown")
+const writeFileAsync = util.promisify(fs.writeFile);
+
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: "input",
+        name: "title",
+        message: "Project title:",
+    },
+    {
+        type: "input",
+        name: "description",
+        message: "Description:: "
+    },
+    {
+        type: "input",
+        name: "installation",
+        message: "Installation: ",
+    },
+    {
+        type: "input",
+        name: "usage",
+        message: "Usage:"
+    },
+    {
+        type: "list",
+        name: "license",
+        message: "Choose license: ",
+        choices: [
+            "MIT",
+            "Open",
+            "Academic",
+            "Apache",
+            "Mozilla",
+            "GNU",
+            "ISC",
+        ]
+    },
+    {
+        type: "input",
+        name: "contributing",
+        message: "Contributing:"
+    },
+    {
+        type: "input",
+        name: "tests",
+        message: "Tests:"
+    },
+    {
+        type: "input",
+        name: "questions",
+        message: "Questions:"
+    },
+    {
+        type: "input",
+        name: "username",
+        message: "GitHub username: "
+    },
+    {
+        type: "input",
+        name: "email",
+        message: "Your email: "
+    }
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+async function writeToFile(fileName, data) {
+    await writeFileAsync(`${fileName}.md`, data);}
 
 // TODO: Create a function to initialize app
-function init() {}
+async function init() {
+    try {
+        const data = await inquirer.prompt(questions);
+        const markdown = generateMarkdown(data);
+        await writeToFile(`${data.title}`, markdown);
+    }
+    catch (e){
+        console.error(e.message);
+    }
+}
 
 // Function call to initialize app
-init();
+init().then(() => console.log('README file generated')).catch(() => console.error('Could not initialize application'));
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,19 +1,82 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  return license ? `![badge](${renderLicenseLink(license)})` : '';
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  return license ? `https://img.shields.io/badge/license-${license}-brightgreen` : '';
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  return `
+## License
+${renderLicenseBadge(license)}
+<br/>
+This application is covered under ${license} license.
+`;
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+  return `
+${data.title ? `# ${data.title}` : ''}
 
+${data.license ? `# ${renderLicenseBadge(data.license)}` : ''}
+
+${data.description ? `
+## Description
+${data.description}
+` : ''}
+
+## Table of Contents
+${data.installation ? `- [Installation](#installation)` : ''}
+${data.usage ? `- [Usage](#usage)` : ''}
+${data.license ? `- [License](#license)` : ''}
+${data.contributing ? `- [Contributing](#contributing)` : ''}
+${data.tests ? `- [Tests](#tests)` : ''}
+${data.questions ? `- [Questions](#questions)` : ''}
+
+${data.installation ? `
+## Installation
+${data.installation}
+` : ''}
+
+${data.usage ? `
+## Usage
+${data.usage}
+` : ''}
+
+${data.license ? `
+${renderLicenseSection(data.license)}
+` : ''}
+
+${data.contributing ? `
+## Contributing
+${data.contributing}
+` : ''}
+
+${data.tests ? `
+## Tests
+${data.tests}
+` : ''}
+
+${data.questions ? `
+## Questions
+${data.questions}
+` : ''}
+
+${data.username ? `
+Find me on GitHub: [${data.username}](https://github.com/${data.username}) <br/>
+` : ''}
+
+${data.email ? `
+Email me with any questions: ${data.email} <br/> 
+` : ''}
 `;
 }
 
